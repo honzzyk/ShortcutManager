3f49ee9133d521879830b3973bb179e64e2e5458
Radek Sedlák <r.sedlak@quadient.com>
2024-04-30 11:46:23 +0200

    version 1.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### v1.0.3
+
+### Fixed
+- fixed d.ts types for handler in `on`, `remove` methods
--- scm.d.ts
+++ scm.d.ts
@@ -2,26 +2,29 @@ declare module SC {
 	interface IShortcutContext {
 
 	}
+
+	type THandler = (normalizedShortcut: string, modifier: number, from: number, originalEvent: KeyboardEvent) => boolean;
+
 	class Manager {
 		context: IShortcutContext;
 
 		constructor(isStatic: boolean);
 
-		create(context: Object, layer?: Window|Document|Element|string): SC.Manager;
+		create(context: Object, layer?: Window | Document | Element | string): SC.Manager;
 
-		on(shortcut: string, handler: () => boolean, isDefault?: boolean): void;
+		on(shortcut: string, handler: THandler, isDefault?: boolean): void;
 
 		activate(): void;
 
 		deactivate(): void;
 
-		remove(shortcut?: string, handler?: () => void): void;
+		remove(shortcut?: string, handler?: THandler): void;
 
 		debugMode(state: boolean): void;
 
 		event(keyboardEvent: KeyboardEvent): boolean;
 
-		exists(keyboardEvent: string|KeyboardEvent): boolean;
+		exists(keyboardEvent: string | KeyboardEvent): boolean;
 
 		destroy(): void;
 	}
